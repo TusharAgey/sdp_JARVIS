8050d0726748454393de30499b78f6247ccc4d46
Tushar Agey <agey.tushar3@gmail.com>
2017-02-19 16:23:10 +0530

    Main page can do Text to speech & can exit the application in a good way

--- app/src/main/java/jarvis/jarvis/Main.java
+++ app/src/main/java/jarvis/jarvis/Main.java
@@ -1,13 +1,37 @@
 package jarvis.jarvis;
 
+import android.content.Context;
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Main extends AppCompatActivity {
+public class Main extends AppCompatActivity implements TextToSpeech.OnInitListener{
+    private TextToSpeech tts;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        tts = new TextToSpeech(this, this);
+        Button buttonOne = (Button) findViewById(R.id.exitButton);
+        buttonOne.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
     }
+    public void onInit(int status) {
+        speakOut();
+        //Intent intent = new Intent(context, oneTimeHelp.class);
+        //startActivity(intent);
+    }
+    private void speakOut() {
+        tts.speak("Welcome to the World of JARVIS", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
 }
