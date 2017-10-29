d10a8b8154240429f922a08e4698f07d0b502b05
Tushar Agey <agey.tushar3@gmail.com>
2017-10-29 10:33:20 +0530

    Created a God activity that allows integration of JARVIS and ATA

--- app/src/main/java/jarvis/jarvis/MegaStart.java
+++ app/src/main/java/jarvis/jarvis/MegaStart.java
@@ -1,20 +1,37 @@
 package jarvis.jarvis;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-
+import android.widget.*;
 public class MegaStart extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Context context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mega_start);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button jarvis = (Button)findViewById(R.id.openJarvis);
+        Button hologram = (Button) findViewById(R.id.openHologram);
+
+        jarvis.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Main.class);
+                startActivity(intent);
+            }
+        });
+
+        hologram.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Log.e("Start", "Hello ATA hologram");
+            }
+        });
     }
 
 }
